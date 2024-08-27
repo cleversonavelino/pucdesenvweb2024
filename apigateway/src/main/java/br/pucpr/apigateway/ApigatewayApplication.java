733fab8fb24ab01e708c30ea81a26252d40b46ee
Avelino <cleverson.avelino@dell.com>
2024-08-27 20:32:31 -0300

    gateway

---
+++ apigateway/src/main/java/br/pucpr/apigateway/ApigatewayApplication.java
@@ -0,0 +1,32 @@
+package br.pucpr.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableEurekaClient
+@Configuration
+public class ApigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p -> p
+						.path("/usuario")
+						.uri("http://localhost:8083/usuario"))
+				//.route( r -> r.path("/**")
+				//		//.filters(f -> f.stripPrefix(1))
+				//		.uri("lb://VEICULO-SERVICE"))
+				.build();
+	}
+}
